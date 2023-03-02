24fca022d79918596f08dfd8124234293a2c0c57
rf-MLquests <renfrederic@gmail.com>
2023-03-02 12:28:16 -0800

    Remove redundant functions

--- Models/random_forest.py
+++ Models/random_forest.py
@@ -1,16 +1,9 @@
 from sklearn.ensemble import RandomForestClassifier
-from Training.training import pre_train_prep, pre_train_prep_compressed
+from Training.training import pre_train_prep
 
 
 def random_forest_model(df):
     x_train, x_test, y_train, y_test = pre_train_prep(df)
-    random_forest = RandomForestClassifier(class_weight='balanced', random_state=1)
-    random_forest.fit(x_train, y_train)
-    return random_forest
-
-
-def compressed_random_forest_model(df):
-    x_train, x_test, y_train, y_test = pre_train_prep_compressed(df)
     random_forest = RandomForestClassifier(class_weight='balanced', random_state=1)
     random_forest.fit(x_train, y_train)
     return random_forest
--- Models/xgboost.py
+++ Models/xgboost.py
@@ -1,16 +1,9 @@
 from xgboost import XGBClassifier
-from Training.training import pre_train_prep, pre_train_prep_compressed
+from Training.training import pre_train_prep
 
 
 def xgboost_model(df):
     x_train, x_test, y_train, y_test = pre_train_prep(df)
-    xgb = XGBClassifier(scale_pos_weight=4, random_state=1, eval_metric='logloss')
-    xgb.fit(x_train, y_train)
-    return xgb
-
-
-def compressed_xgboost_model(df):
-    x_train, x_test, y_train, y_test = pre_train_prep_compressed(df)
     xgb = XGBClassifier(scale_pos_weight=4, random_state=1, eval_metric='logloss')
     xgb.fit(x_train, y_train)
     return xgb
